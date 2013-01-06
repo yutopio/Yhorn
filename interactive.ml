1eab9092e798d93b15095d559fbe9a504a286821
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2013-01-07 00:10:42 +0900

    Get filename as startup argument

--- interactive.ml
+++ interactive.ml
@@ -3,8 +3,14 @@ open Types
 open Horn
 
 let main _ =
+  let input =
+    match Array.length (Sys.argv) with
+      | 1 -> stdin
+      | 2 -> let filename = Sys.argv.(1) in open_in (filename)
+      | _ -> assert false in
+
   let clauses, merges =
-    Parser.inputUnit Lexer.token (Lexing.from_channel stdin) in
+    Parser.inputUnit Lexer.token (Lexing.from_channel input) in
 
   print_endline (
     String.concat "\n" (List.map printHorn clauses) ^ "\n" ^
