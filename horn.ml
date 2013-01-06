6e905e9b27a357fd7f243fb4058f3131ad29e9e6
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2013-01-07 08:02:44 +0900

    Split equation to LEQ inequalities

--- horn.ml
+++ horn.ml
@@ -19,7 +19,6 @@ let generatePexprMergeConstr (op1, coef1) (op2, coef2) =
     let f () = "q" ^ string_of_int (new_id ()) in
     f (), f () in
   let c3 = Expr(GT , M.add q1 1 M.empty) in (* q1  > 0 *)
-  let c1 = Expr(NEQ, M.add q2 1 M.empty) in (* q2 != 0 *)
   let c2 = Expr(GT , M.add q2 1 M.empty) in (* q2  > 0 *)
 
   (* Coefficients of both interpolants must be the same *)
@@ -28,22 +27,8 @@ let generatePexprMergeConstr (op1, coef1) (op2, coef2) =
     let [v1;v2] = List.map (M.findDefault M.empty k) [coef1;coef2] in
     (Expr(EQ, (mul q1 v1) -- (mul q2 v2)) :: r)) [c3] vars in
 
-  (* Check weight variables those for making an interpolant LTE. *)
-  let f x =
-    let p = M.fold (fun k v p -> if v = LTE then k :: p else p) x [] in
-    let eq = List.fold_left (fun c x ->
-      (Expr (EQ, M.add x 1 M.empty)) :: c) [] p in
-    (p, eq) in
-  let (p1lte, i1eq), (p2lte, i2eq) = (f op1), (f op2) in
-
-  let [c3;i1eq;i2eq] = List.map (fun x -> And x) [c3;i1eq;i2eq] in
-
   (* Constraint for making both interpolant the same operator. *)
-  match p1lte, p2lte with
-    | [], [] -> c1 &&& c3
-    | _, [] -> c1 &&& c3 &&& i1eq
-    | [], _ -> c1 &&& c3 &&& i2eq
-    | _ -> c3 &&& (i1eq <=> i2eq) &&& (i1eq ==> c1) &&& ((!!!i1eq) ==> c2)
+  And (c2 :: c3)
 
 let buildGraph clauses =
   (* TODO: Handle props without assumptions, and props that imply nothing. *)
