549a0e0c4b77f97c670fe0db50d42dc2ffabb548
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2013-04-22 22:59:24 +0900

    Changing for constraint propagation over DAGs * All fork nodes are cutpoints * Every tree generates its constraint

--- horn.ml
+++ horn.ml
@@ -76,17 +76,13 @@ let buildGraph clauses =
 module GI = Graph.Persistent.Digraph.Concrete(Integer)
 module MV = Map.Make(G.V)
 module SV = Set.Make(G.V)
-module TraverseGI = Graph.Traverse.Dfs(GI)
 module TopologicalGI = Graph.Topological.Make(GI)
 
-let cutGraph g =
+let solveGraph g =
   let cutpoints =
     G.fold_vertex (fun v s ->
       if G.in_degree g v > 1 then SV.add v s else s) g SV.empty in
-  Types.Display.highlight_vertices := cutpoints;
-
-  (* If no cutpoints are defined, do nothing. *)
-  if SV.cardinal cutpoints = 0 then [g] else (
+  (* DEBUG: *) Types.Display.highlight_vertices := cutpoints;
 
   (* Create a mapping from a vertex to an integer ID, and extract vertices to
      start traversal. They have no incoming edges. *)
@@ -128,7 +124,8 @@ let cutGraph g =
   let cmpMap, link = step start SV.empty p [] in
 
   (* Verify that those components don't have mutural dependency. *)
-  let linkG = List.fold_left (fun g (x, y) -> GI.add_edge g x y) GI.empty link in
+  let linkG = List.fold_left (
+    fun g (x, y) -> GI.add_edge g x y) GI.empty link in
 
   (* Fold over edges to create new graphs for each component. *)
   let components = G.fold_edges_e (fun e m ->
@@ -142,264 +139,148 @@ let cutGraph g =
       with Not_found -> G.empty in
     MI.add uid (G.add_edge_e gg e) m) g MI.empty in
 
+  (* Calculate roots. *)
+  let roots = SV.fold (fun v -> MI.add (lookup v) v) cutpoints MI.empty in
+
   let components =
-    (* NOTE: No need of List.rev *)
     TopologicalGI.fold (fun v l -> v :: l) linkG [] |>
     MI.fold (fun k _ l -> if List.mem k l then l else k :: l) components |>
-    List.map (fun k -> MI.find k components) in
+    List.map (fun k -> (MI.find k components), (MI.find k roots)) |>
+    List.rev in
+
+  let addPcoef = M.addDefault M.empty (fun m (k, v) -> M.add k v m) in
+
+  let addLinear x =
+    mapFormula normalizeExpr |-
+    splitNegation |-
+    convertToNF false |-
+
+    (* TODO: Support of disjunctions.
+       We now only consider first conjunction in DNF. *)
+    (function
+    | [x] -> x
+    | [] -> assert false
+    | x::_ ->
+      print_endline "Omitting conjunctions..."; x) |-
+
+    (* Filter the inserted tautologies 0=0 during the process. *)
+    List.filter (fun x -> x <> (LTE, M.empty)) |-
+
+    (* Add to the variable maps for applying Farkas' Lemma. *)
+    List.fold_left (fun (m, pis) (op, coef) ->
+      let pi = Id.create () in
+
+      (* Building an coefficient mapping in terms of variables. *)
+      (M.fold (fun k v -> addPcoef k (pi, v)) coef m),
+
+      (* TODO: Linear expression must be normalized to LTE. No EQ allowed. *)
+      (match op with
+      | LTE -> pi :: pis
+      | _ -> assert false)) x in
+
+  (* TODO: Currently supported template is only [1] in Module Template type.
+           Any larger templates should be available in future. *)
+
+  (* Conjunction operation on Some. Similar to Maybe Monad. *)
+  let (&&&) x = function
+    | None -> Some x
+    | Some y -> Some (x &&& y) in
 
   (* Compute at each vertex linear expressions those are ancestors of it. *)
-  let rec computeAncestors visited v =
+  let rec computeAncestors visited v g =
     if MV.mem v visited then
       (* If the vertex is already visited, just return the saved result. *)
       visited, (MV.find v visited)
     else
-      G.fold_succ_e (fun e (visited, la) ->
-        (* Conjunction operation on Some. Similar to Maybe Monad. *)
-        let (&&&) x = function
-          | None -> Some x
-          | Some y -> Some (x &&& y) in
-
-        let u = G.E.dst e in
-        match G.E.label e, G.V.label u with
+      let visited, (constrs, (m, pis)) =
+        G.fold_succ_e (fun e (visited, (constrs, (m, pis))) ->
+          let u = G.E.dst e in
+          match G.E.label e, G.V.label u with
           | None, LinearExpr ex ->
             (* No renaming should occur for linear expressions. *)
-            visited, (ex &&& la)
+            (* TODO: We should consider disjunctive linear expressions. *)
+            visited, (constrs, (addLinear (m, pis) ex))
           | Some rename, PredVar p ->
-            let m = ref (
+            (* TODO: We should consider disjunctive templates. *)
+            (* TODO: Support parameterized operators. *)
+            let visited, (constr, (pop, pcoef)) =
+              computeAncestors visited u g in
+
+            let rename = ref (
               List.fold_left (fun m (x, y) -> M.add x y m) M.empty rename) in
-            if MV.mem u visited then
-              let x = mapFormula (renameExpr m) (MV.find u visited) in
-              visited, (x &&& la)
-            else
-              let visited', x = computeAncestors visited u in
-              let x = mapFormula (renameExpr m) x in
-              visited', (x &&& la)
-      ) g v (visited, None) |>
-      (function
-        | _, None ->
-          (* All nodes without incoming edges are linear expression nodes. *)
-          assert false
-        | visited, Some y ->
-          (* Add traversal result. *)
-          MV.add v y visited, y) in
+            let f k v = addPcoef (renameVar rename k) (v, 1) in
 
-  let rec step visited next =
-    if SV.is_empty next then visited
-    else
-      let u = SV.choose next in
-      let next = SV.remove u next in
-      let visited, _ = computeAncestors visited u in
-      step visited next in
+            (* Building an coefficient mapping in terms of variables. *)
+            visited, ((constr &&& constrs), ((M.fold f pcoef m), pis))
+          | _ -> assert false
+        ) g v (visited, (None, (M.empty, []))) in
 
+      let (m, pis), (pop, pcoef) =
+        match G.V.label v with
+        | LinearExpr e ->
+          (* Add the current linear expression for Farkas' target. *)
+          (addLinear (m, pis) (!!! e)),
+
+          (* Don't care about the returning template. *)
+          (M.empty, M.empty)
+
+        | PredVar (p, param) ->
+          (* Give a template. *)
+          let pcoef =
+            List.fold_left (fun m x ->
+              M.add x (Id.create ()) m) M.empty param |>
+            M.add Id.const (Id.create ()) in
+
+          (* Add the atomic current predicate template for Farkas' target. *)
+          let m =
+            M.fold (fun k v -> addPcoef k (v, -1)) pcoef m |>
+            addPcoef Id.const (Id.const, 1) in
+
+          (m, pis),
+          (* TODO: Currently all template are considered to be LTE. *)
+          (M.fold (fun _ v -> M.add v LTE) pcoef M.empty, pcoef) in
+
+      let constrs =
+        (* Every linear inequality must be weighted non-negative. *)
+        List.fold_left (fun l pi ->
+          Expr (GTE, M.add pi 1 M.empty) :: l) [] pis |>
+
+        (* Additionally, add constraints to make totals on every
+           coefficients zero. *)
+        M.fold (fun k v c ->
+          Expr ((if k = Id.const then GT else EQ), v) :: c) m |>
+
+        (fun x -> match And x &&& constrs with
+        | None -> Expr (EQ, M.empty)
+        | Some x -> x)
+
+        (* TODO: Simplification will be done here. *)
+      in
+
+      let ret = constrs, (pop, pcoef) in
+      (MV.add v ret visited), ret in
+
+  (* TODO: If you have multiple bottom vertices, check all constraints from
+           them. *)
   let leaves =
-    step MV.empty cutpoints |>
+    List.fold_left (fun x (g, root) ->
+      computeAncestors x root g |> fst
+    ) MV.empty components |>
     MV.filter (fun k _ -> SV.mem k cutpoints) in
   let u = MV.cardinal leaves in
 
-  (* Attach ancestor linear expressions to cutpoints in every subgraph. *)
-  let components = List.map (
-    SV.fold (fun v g ->
-      if G.mem_vertex g v && G.out_degree g v = 0 then (
-        assert (MV.mem v leaves);
-        let expr = MV.find v leaves in
-        let u = G.V.create (LinearExpr expr) in
-        G.add_edge g v u)
-      else
-        (* Other cases: Do nothing.
-           not mem_vertex ... Doesn't belong to this subgraph.
-           out_degree = 0 ... It's a root cutpoint.
-           otherwise ........ Invalid cutpoint specification. Ignored. *)
-        g)
-      cutpoints) components in
+  let check =
+    SV.fold (fun x -> (&&&) (MV.find x leaves |> fst)) start None |>
+    function
+    | None -> Expr (EQ, M.empty)
+    | Some x -> x in
 
-  (* Return splitted graphs. *)
-  components)
-
-let flattenGraph g =
-  G.fold_vertex (fun v l ->
-    (* TODO: This algorithm cannot find the self-looping predicate variable node
-       which has no children. *)
-    if G.in_degree g v = 0 then v :: l else l) g [] |>
-
-  (* Traverse the graph from given starting points to make trees. *)
-  List.map (fun v ->
-    (* Keep track of linear expressions with giving a unique ID. *)
-    let laGroups = ref MI.empty in
-
-    (* After splitting a graph into a tree, one predicate variable may appear
-       multiple times in the tree. Thus, each appearance will be remembered by
-       the ID. `predMap` tracks the mapping between such predicate ID and linear
-       expression IDs appearing above the predicate. `predCopies` remembers the
-       correspondance between the original name of predicate variable and
-       predicate IDs. *)
-    let predMap = ref M.empty in
-    let predCopies = ref M.empty in
-
-    let g, v, nameMap = match G.V.label v with
-      | LinearExpr e ->
-        let nameMap = ref M.empty in
-        ignore(mapFormula (renameExpr nameMap) e);
-        g, v, M.fold (fun k v -> M.add k k) !nameMap M.empty
-      | PredVar (_, l) ->
-        let top = G.V.create (LinearExpr (Expr (EQ, M.empty))) in
-        G.add_edge_e g (G.E.create top (Some (
-          List.map (fun x -> (x, x)) l)) v), top, M.empty
-    in
-
-    let rec f v nameMap g' =
-      let registerLa la =
-        let key = MI.cardinal !laGroups in
-        laGroups := MI.add key la !laGroups; key in
-
-      let (g', u's, u'keys, _) = G.fold_succ_e (fun e
-        (g', u's, u'keys, hadLa) ->
-
-        let u = G.E.dst e in
-        match G.V.label u, G.E.label e with
-          | PredVar _, Some bindings ->
-            (* Bindings of predicate variables are converted to linear equations
-               so that it can be treated easy. Note that the binder and argument
-               will have different names even if they have the same one before
-               conversion.*)
-            let nameMap = List.fold_left (fun m (x, y) ->
-              let y' = renameVar nameMap y in
-              M.add x y' m) M.empty bindings in
-
-            (* Traverse children first to unify predicate variable nodes. *)
-            let (g', u', u'keys') = f u (ref nameMap) g' in
-            g', ((u', Some bindings) :: u's), u'keys' @ u'keys, hadLa
-
-          | LinearExpr e, None ->
-            if hadLa then
-              (* NOTE: Linear expressions must appear only once. *)
-              assert false
-            else
-              let laKey = registerLa (e, !nameMap) in
-              g', (G'.V.create (La laKey), None) :: u's, laKey :: u'keys, true
-
-          | _ -> assert false
-      ) g v (g', [], [], false) in
-
-      let v' = G'.V.create (
-        match G.V.label v with
-          | PredVar (p, param) ->
-            let pid = Id.create () in
-            predMap := M.add pid (param, u'keys) !predMap;
-            predCopies := M.addDefault [] (fun l x -> x :: l) p pid !predCopies;
-            Pid pid
-
-          | LinearExpr e ->
-            (* NOTE: The vertex 'v' must be the root of traversal. *)
-            La (registerLa (!!!e, !nameMap))) in
-
-      (* Add edges between new left-hand nodes and right-hand node. *)
-      let g' = List.fold_left (fun g' (u', lb) ->
-        G'.E.create u' lb v' |> G'.add_edge_e g') g' u's in
-      g', v', u'keys
-    in
-    let (g', _, _) = f v (ref nameMap) G'.empty in
-    g', !laGroups, !predMap, !predCopies)
-
-let solveTree g =
-  (* Assign a template to all predicate variables. *)
-  (* TODO: Currently supported template is only [1] in Module Template type.
-           Any larger templates should be available in future. *)
-  let templ =
-    G.fold_vertex (fun v m ->
-      match G.V.label v with
-      | LinearExpr _ -> m
-      | PredVar (p, param) ->
-        let rename =
-          List.fold_left (fun m x -> M.add x (Id.create ()) m) M.empty param |>
-          M.add Id.const (Id.create ()) in
-        M.add p (param, rename) m) g M.empty in
-
-  let constrs = G.fold_vertex (fun v x ->
-    let l1, l2 = G.fold_succ_e (fun e (l1, l2) ->
-      match G.E.label e, G.V.label (G.E.dst e) with
-      | None, LinearExpr e ->
-        print_endline ("LinE  " ^ printFormula printExpr e);
-        e :: l1, l2
-      | Some rename, PredVar (p, param) ->
-        l1,
-        (* TODO: Currently all template are considered to be LTE. *)
-        let rename = List.fold_left (fun m (a, b) ->
-          M.add a b m) M.empty rename in
-        let _, pm = M.find p templ in
-        let pm = M.fold (fun k -> M.add (
-          if k = Id.const then Id.const
-          else M.find k rename)) pm M.empty in
-        let pexpr = M.empty, M.map (fun x -> M.add x 1 M.empty) pm in
-        (print_endline ("Pred  " ^ printPexpr pexpr);
-        pm :: l2)
-    ) g v ([], []) in
-
-    if l1 = [] && l2 = [] then x else (
-
-    let addPcoef = M.addDefault M.empty (fun m (k, v) -> M.add k v m) in
-
-    let addLinear x =
-      mapFormula normalizeExpr |-
-      splitNegation |-
-      convertToNF false |-
-
-      (* TODO: Support of disjunctions.
-         We now only consider first conjunction in DNF. *)
-      (function
-      | [x] -> x
-      | [] -> assert false
-      | x::_ ->
-        print_endline "Omitting conjunctions..."; x) |-
-
-      (* Filter the inserted tautologies 0=0 during the process. *)
-      List.filter (fun x -> x <> (LTE, M.empty)) |-
-
-      (* Add to the variable maps for applying Farkas' Lemma. *)
-      List.fold_left (fun (m, pis) (op, coef) ->
-        let pi = Id.create () in
-
-        (* Building an coefficient mapping in terms of variables. *)
-        (M.fold (fun k v -> addPcoef k (pi, v)) coef m),
-
-        (* TODO: Linear expression must be normalized to LTE. No EQ allowed. *)
-        (match op with
-        | LTE -> pi :: pis
-        | _ -> assert false)) x in
-
-    let m, pis = List.fold_left addLinear (M.empty, []) l1 in
-
-    let m = List.fold_left (fun m pcoef ->
-      (* Building an coefficient mapping in terms of variables. *)
-      (M.fold (fun k v -> addPcoef k (v, 1)) pcoef m))
-      m l2 in
-
-    let m =
-      match G.V.label v with
-      | LinearExpr e ->
-        addLinear (m, []) (!!! e) |> fst
-      | PredVar (p, param) ->
-        (* TODO: Currently all template are considered to be LTE. *)
-        let _, pcoef = M.find p templ in
-
-        (* Building an coefficient mapping in terms of variables. *)
-        M.fold (fun k v -> addPcoef k (v, -1)) pcoef m |>
-        addPcoef Id.const (Id.const, 1) in
-
-    (* Every linear inequality must be weighted non-negative. *)
-    List.fold_left (fun l pi -> Expr (GTE, M.add pi 1 M.empty) :: l) x pis |>
-
-    (* Additionally, add constraints to make totals on every
-       coefficients zero. *)
-    M.fold (fun k v c ->
-      Expr ((if k = Id.const then GT else EQ), v) :: c) m
-    )) g [] in
-
+  (* TODO: Backward compatibility. *)
   M.map (fun (param, v) -> param,
     [[M.fold (fun _ v -> M.add v LTE) v M.empty,
-      M.map (fun v -> M.add v 1 M.empty) v]]) templ,
+      M.map (fun v -> M.add v 1 M.empty) v]]) (assert false),
   [ Id.create () ],
-  MI.add 1 (And constrs) MI.empty
+  MI.add 1 check MI.empty
 
 let simplifyPCNF clauses =
   let simplifyPDF exprs =
@@ -582,52 +463,13 @@ let solve clauses =
   assert (not (Traverser.has_cycle g));
 
   (* Compute suitable cutpoints. *)
-  let subgraphs = cutGraph g in
+  let _ = solveGraph g in
 
-  (* DEBUG: Show the constructed graph. *)
-  display_with_gv (Operator.mirror g);
+  (* Solving constraints. *)
+  let sol = assert false in
 
-  List.fold_left (fun sol g ->
-    print_endline "Solving subgraph";
-    display_with_gv (Operator.mirror g);
-
-    (* Replace known nodes with linear expressions. *)
-    let vReplace = G.fold_vertex (fun v m ->
-      if G.in_degree g v = 0 then
-        match G.V.label v with
-        | PredVar (p, args) ->
-          let (param, sol) = M.find p sol in
-          let rename = ref (listFold2 (
-            fun m u v -> M.add u v m) M.empty param args) in
-          let body = LinearExpr (mapFormula (renameExpr rename) sol) in
-          MV.add v (G.V.create body) m
-        | _ -> m
-      else m) g MV.empty in
-    let g = MV.fold (fun k v ->
-      G.fold_succ_e (fun e g' ->
-        G.add_edge_e g' (G.E.create v (G.E.label e) (G.E.dst e))
-      ) g k |-
-      (fun g' -> G.remove_vertex g' k)
-    ) vReplace g in
-
-    print_endline "Replaced precomputed solution";
-    display_with_gv (Operator.mirror g);
-
-    solveTree g |>
-
-    (fun (m, i, c) ->
-      M.fold (fun k (p, v) -> M.add k (p, simplifyPCNF v)) m M.empty,
-      (i, Puf.create (List.length i), c)) |>
-
-    getSolution |>
-
-    (* TODO: Simple merge is not appropriate here. *)
-    M.simpleMerge sol |>
-
-    (fun x -> print_endline (printHornSol x); x)
-  ) M.empty subgraphs |>
-
-  (fun x -> M.fold (fun k -> M.add (M.findDefault k k pm)) x M.empty)
+  (* Rename back to original predicate variable names. *)
+  M.fold (fun k -> M.add (M.findDefault k k pm)) sol M.empty
 
 let solve clauses unify =
   (* NYI: Unification [tryUnify] ? *)
