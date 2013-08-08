ad331cc65181c8ba189f2ff3358605f3032ac739
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2013-08-08 23:56:20 +0900

    Changes ongoing for new algorithm

--- horn.ml
+++ horn.ml
@@ -7,27 +7,9 @@ open MTypes
 open Types
 open Util
 
+let bot = LinearExpr (Term (LTE, M.add Id.const 1 M.empty))
+
 let createRename = List.fold_left2 (fun m k v -> M.add k v m) M.empty
-
-let preprocLefthand =
-  Nf.dnf_of_formula |-
-  List.map (fun x ->
-    let pvars, la =
-      List.fold_left (fun (pvars, la) ->
-        function
-        | LinearExpr x -> pvars, Some (
-          let x = normalizeOperator x in
-          match la with
-          | None -> x
-          | Some y -> x &&& y)
-        | PredVar pvar -> (pvar :: pvars), la) ([], None) x in
-
-    match la with
-    | None -> [pvars, None]
-    | Some la ->
-      Nf.dnf_of_formula la |>
-      List.map (fun x -> pvars, Some (And (List.map (fun x -> Term x) x)))) |-
-  List.flatten
 
 let simplifyCNF clauses =
   let simplifyDF =
@@ -60,11 +42,31 @@ let simplifyCNF clauses =
 
   contradiction, List.sort_distinct (List.compare Expr.comp) ret
 
+let preprocLefthand =
+  Nf.dnf_of_formula |-
+  List.map (fun x ->
+    let pvars, la =
+      List.fold_left (fun (pvars, la) ->
+        function
+        | LinearExpr x -> pvars, Some (
+          let x = normalizeOperator x in
+          match la with
+          | None -> x
+          | Some y -> x &&& y)
+        | PredVar pvar -> (pvar :: pvars), la) ([], None) x in
+
+    match la with
+    | None -> [pvars, None]
+    | Some la ->
+      Nf.dnf_of_formula la |>
+      List.map (fun x -> pvars, Some (And (List.map (fun x -> Term x) x)))) |-
+  List.flatten
+
 let buildGraph clauses =
-  let bot = LinearExpr (Term (LTE, M.add Id.const 1 M.empty)) in
   let (cs_p, cs_l) =
     List.map (fun (lh, rh) ->
       List.map (fun lh -> lh, rh) (preprocLefthand lh)) clauses |>
+    (* TODO: Optimize starting here... *)
     List.flatten |>
     List.partition (fun (_, rh) ->
       match rh with
@@ -92,6 +94,7 @@ let buildGraph clauses =
       List.map (fun la -> (pvars, la), bot) |>
       (@) ret) [] cs_l in
   let clauses = cs_p @ cs_l in
+  (* TODO: Optimize ending here... *)
 
   (* Create predicate symbol vertices in advance. *)
   let addVp rh vp (p, l) =
@@ -113,15 +116,17 @@ let buildGraph clauses =
       M.add p (G.V.create (HT (PredVar (p, binders)))) vp in
 
   (* Create vertices. *)
+  (* TODO: Can be processed first for performance. *)
   let predVertices = List.fold_left (fun vp ((lh, _), rh) ->
     let vp =
       match rh with
       | PredVar pvar -> addVp true vp pvar
       | _ -> vp in
     List.fold_left (addVp false) vp lh) M.empty clauses in
+  let preds = M.keys predVertices in
 
   (* Add implication relations on the Horn graph. *)
-  let v_bot = G.V.create (HT bot) in
+  let root = G.V.create (HT bot) in
   List.fold_left (fun g ((pvars, la) as lh, rh) ->
     let (pvars, la), src =
       match rh with
@@ -131,7 +136,7 @@ let buildGraph clauses =
         let rename = ref (createRename binder binder') in
         ((List.map (fun (p, args) -> p, renameList rename args) pvars),
         (maybeApply (Formula.map (renameExpr rename)) la)), src
-      | LinearExpr _ -> lh, v_bot in
+      | LinearExpr _ -> lh, root in
 
     let arrow = G.V.create Arrow in
     let g = G.add_edge_e g (G.E.create src None arrow) in
@@ -181,7 +186,10 @@ let buildGraph clauses =
           Term (LTE, M.add Id.const 1 M.empty)))) in
         G.add_edge_e g (G.E.create v None bot)
       else g in
-    g) predVertices
+    g) predVertices |>
+
+  (* Return with the root and pred names. *)
+  (fun g -> g, root, preds)
 
 let rootE =
   let dummy = G.V.create (HT (LinearExpr (Term (EQ, M.empty)))) in
@@ -208,28 +216,6 @@ module MEL = Map.Make(EL)
 module SV = Set.Make(G.V)
 module TopologicalGI = Graph.Topological.Make(GI)
 
-let findRoot g =
-  assert (not (G.is_empty g));
-
-  let roots = G.fold_vertex (fun v s ->
-    if G.in_degree g v = 0 then SV.add v s else s) g SV.empty in
-
-  let _, root as ret =
-    if SV.cardinal roots = 1 then
-      g, (SV.choose roots)
-    else
-      let arrow = G.V.create Arrow in
-      let root = G.V.create (HT (LinearExpr (Term (LTE, M.empty)))) in
-      let g =
-        G.add_edge g root arrow |>
-        SV.fold (fun v g -> G.add_edge g arrow v) roots
-      in
-      g, root
-  in
-
-  rootE := G.E.create root None root;
-  ret
-
 let assignParameters assign =
   M.map (fun v -> M.fold (fun k v -> (+) ((M.findDefault 1 k assign) * v)) v 0)
 
@@ -243,7 +229,7 @@ type constr = G.E.t list * constrTypes
 exception Unsatisfiable of constr list
 exception No_growth
 
-let solveGraph (g, root) =
+let solveGraph (g, root) sol_templ =
   let cutpoints =
     G.fold_vertex (fun v s ->
       if G.in_degree g v > 1 then SV.add v s else s) g SV.empty in
@@ -344,30 +330,6 @@ let solveGraph (g, root) =
   let components = linkOrder |>
     List.map (fun k -> (MI.find k roots), (MI.find k components)) in
 
-  (* For simplification purpose, travese the component link graph and compute
-     appropriate quantifiers. *)
-  let (fvs, use) = List.fold_left (fun (fvs, use) x ->
-    let v = MI.find x roots in
-    let fv =
-      match G.V.label v with
-      | HT (PredVar (p, param)) -> M.find p templ |> M.values
-      | _ -> [] in
-    let use' = fv @ (MI.findDefault [] x use |> List.sort_distinct Id.compare) in
-    (MI.add x fv fvs),
-    (GI.fold_succ (fun y -> MI.addDefault [] (@) y use') linkG x use)
-  ) (MI.empty, MI.empty) linkOrder in
-  let quant = List.fold_left (fun quant x ->
-    let use = MI.findDefault [] x use in
-    let dup = List.fold_left (fun m x ->
-      M.addDefault 0 (+) x 1 m) M.empty use in
-    let l1 = M.fold (fun k v l -> if v > 1 then k :: l else l) dup [] in
-    let l2 = MI.find x fvs in
-    let l3 =
-      GI.fold_succ (fun y -> (@) (MI.find y quant)) linkG x [] |>
-      List.filter (fun x -> List.mem x use) in
-    MI.add x (List.sort_distinct Id.compare (l1 @ l2 @ l3)) quant
-  ) MI.empty (List.rev linkOrder) in
-
   let addPcoef e =
     let add (es, m) (k, v) =
       (match e with None -> es | Some e -> e :: es),
@@ -387,29 +349,9 @@ let solveGraph (g, root) =
     M.fold (fun k v -> M.add k (renameVar map v)) pcoef M.empty,
     List.map (fun (x, y) -> x, Formula.map (renameExpr map) y) constr in
 
-  (* Compute at each vertex linear expressions those are ancestors of it. *)
-  let rec computeAncestors templs v e g =
-    if MV.mem v templs then
-      (* If the vertex is already registered in the template list, it is a
-         forking predicate variable node. *)
-      let deltaMap, defDelta, dup = MV.find v templs in
-
-      (* If the predicate is specified for duplication, rename it. *)
-      let pcoef, constrs =
-        try duplicate (ME.find e deltaMap)
-        with Not_found -> (if dup then duplicate else id) defDelta
-      in
-
-      if dup then
-        let constrs = List.map (fun ((es, a), b) ->  (e :: es, a), b) constrs in
-        MV.add v [e, (pcoef, constrs)] MV.empty
-      else
-        let exprs = List.map snd constrs |> List.reduce (&&&) in
-        (* TODO: Simplification based on quantifiers *)
-        MV.add v [e, (pcoef, [([], Simplified v), exprs])] MV.empty
-    else
-      let x = G.fold_succ_e (fun e () ->
-        let arrow = G.E.dst e in
+  let rec gen_constr ret v no_simp =
+    let ret =
+      G.fold_succ (fun arrow ret ->
         let ret, constrs, (m, pis) =
           G.fold_succ_e (fun e (ret, constrs, (m, pis as m_pis)) ->
             let u = G.E.dst e in
@@ -417,41 +359,38 @@ let solveGraph (g, root) =
             | None, HT (LinearExpr ex) ->
               (* TODO: Optimization *)
               let [x] = Nf.dnf_of_formula ex in
-	      assert (pis = []);
-              let x = List.map snd x in
+              assert (pis = []);
+              let x = List.map snd x in (* Ignore LTE operator *)
               let m_pis = List.fold_left (addLinear (Some e)) m_pis x in
 
               (* No renaming should occur for linear expressions. *)
               ret, constrs, m_pis
 
             | Some rename, HT (PredVar (p, param)) ->
-              let ret' = computeAncestors templs u e g in
+              let ret' = gen_constr ret u no_simp in
 
-              (* TODO: We should consider disjunctive templates. *)
-              let [_, (pcoef, constr)] = MV.find u ret' in
-
-              (* Merging returning template. *)
-              let ret = MV.merge (fun _ -> maybeAdd (@)) ret ret' in
+              let constr = MV.find u ret' in
               let constrs = constr @ constrs in
 
               let rename = ref rename in
               let f k v = addPcoef (Some e) (renameVar rename k) (v, 1) in
 
               (* Building an coefficient mapping in terms of variables. *)
+              let pcoef = M.find p templ in
               ret, constrs, ((M.fold f pcoef m), pis)
 
             | _ -> assert false
-          ) g arrow (MV.empty, [], (M.empty, [])) in
+          ) g arrow (ret, [], (M.empty, [])) in
 
-        let m, pcoef, la =
+        let m, la, quants =
           match G.V.label v with
           | HT (LinearExpr _) -> (* bot *)
-	    m, M.empty, true
+	    m, true, []
           | HT (PredVar (p, param)) ->
             (* Add the atomic current predicate template for Farkas' target. *)
             let pcoef = M.find p templ in
             let m = M.fold (fun k v -> addPcoef None k (v, -1)) pcoef m in
-            m, pcoef, false
+            m, false, M.values pcoef
 	  | _ -> assert false in
 
         let constrs =
@@ -466,211 +405,35 @@ let solveGraph (g, root) =
             let constr = ([], Coef edges), Term (op, coefs) in
             constr :: c) m in
 
-        ignore(MV.add v [e, (pcoef, constrs)] ret)
-      ) g v () in
+        MV.add arrow constrs ret
+      ) g v ret in
 
-      assert false
-  in
+    let constrs = G.fold_succ (fun arrow -> (@) (MV.find arrow ret)) g v [] in
 
-  (* Create templates and constraints for all predicate variables over the
-     graph. *)
-  let rootTempls =
-    List.fold_left (fun templ (root, g) ->
-      let templ' = computeAncestors (MV.map fst templ) root !rootE g in
-
-      let [_, (pcoef, constrs)] = MV.find root templ' in
-
-      MV.add root ((ME.empty, (pcoef, constrs), false), templ') templ
-    ) MV.empty components in
-
-  (* Generate split tree. *)
-  let rootV = GV.V.create root in
-  let st = GV.add_vertex GV.empty rootV in
-
-  let trySolve st =
-    (* Traverse the split tree to generate root constraint. *)
-    let rec step v =
-      let mvv, me = GV.fold_succ_e (fun e (mvv, me) ->
-        let u = GV.E.dst e in
-        let mvv' = step u in
-        MVV.simpleMerge mvv mvv',
-        ME.addDefault [] (fun x y -> y :: x)
-          (GV.E.label e) (GV.V.label u, MVV.find u mvv') me
-      ) st v (MVV.empty, ME.empty) in
-
-      let v' = GV.V.label v in
-      let g' = List.assoc v' components in
-
-      let predTempls =
-        let roots = List.map fst components in
-        let keys = List.take (List.index_of v' roots) roots in
-        MV.map fst rootTempls |>
-        MV.filter (fun k _ -> List.mem k keys) in
-
-      let me = ME.fold (fun e l me ->
-        let templs =
-          List.fold_left (fun mv (v, me) ->
-            (* NOTE: The same v won't appear more than once. *)
-            let (_, templ, _) = MV.find v mv in
-            let me = ME.map fst me in
-            MV.add v (me, templ, true) mv
-          ) predTempls l in
-
-        assert (not (MV.mem v' templs));
-        let templ' = computeAncestors templs v' e g' in
-
-        let [_, (pcoef, constr)] = MV.find v' templ' in
-
-        ME.add e ((pcoef, constr), templ') me) me ME.empty in
-      MVV.add v me mvv
+    let quants =
+      match G.V.label v with
+      | HT (LinearExpr _) -> (* bot *) []
+      | HT (PredVar (p, param)) -> M.find p templ |> M.values
+      | _ -> assert false
     in
 
-    let constrTree = step rootV in
-    let rec step v (op, coef) first =
-      let f v =
-        let (_, x, _), templ = MV.find v rootTempls in
-        v, (x, templ), MV.empty in
-      let v, ((pcoef, constrs), templ), subprobl =
-        match v with
-        | `A (e, vv) ->
-          let c = MVV.find vv constrTree in
-          let v = GV.V.label vv in (
-            try
-              v,
-              ME.find e c,
-              GV.fold_succ_e (fun e' m ->
-                if GV.E.label e' = e then
-                  let u = GV.E.dst e' in
-                  MV.add (GV.V.label u) (u, MVV.find u constrTree) m
-                else m) st vv MV.empty
-            with Not_found -> f v)
-        | `B v -> f v in
+    let constr =
+      if quants = [] || List.mem v no_simp then
+        (* No simplification. *)
+        constrs
+      else
+        let quants = List.fold_left (fun a b -> S.add b a) S.empty quants in
+        let constrs =
+          And (List.map snd constrs) |>
+          AtpInterface.integer_qelim quants in
+        [([], Simplified v), constrs]
+    in
 
-      let constr =
-        (* TODO: Parameterized operator. *)
-        M.merge (fun _ p v ->
-          match p, v with
-          | None, None -> assert false
-          | None, Some 0 -> None
-          | None, Some v -> failwith no_sol
-          | Some p, None
-          | Some p, Some 0 -> Some (Term (EQ, M.add p 1 M.empty))
-          | Some p, Some v ->
-            Some (Term (EQ, M.add p 1 (M.add Id.const (-v) M.empty)))
-        ) pcoef coef |>
-        M.values in
-      let constrs =
-        if List.length constr = 0 then constrs
-        else (([], Binding), And constr) :: constrs in
+    MV.add v constr ret
+  in
 
-      let split_tag =
-        List.fold_left (fun (constrs, m) (tag, ex) ->
-          let name = string_of_int (List.length m) in
-          (name, ex) :: constrs,
-          (name, tag) :: m) ([], []) in
-
-      (* Once the root constraint become satisfiable, all subproblems
-         should have a solution. *)
-      let constrs, symbol_map = split_tag constrs in
-      let sol =
-        try Z3interface.solve constrs
-        with Z3interface.Unsatisfiable x ->
-          let uc_tags = List.map (fun x -> List.assoc x symbol_map) x in
-          raise (Unsatisfiable (List.sort_distinct compare uc_tags)) in
-
-      MV.fold (fun k pexprs sols ->
-        match G.V.label k; assert false with
-        | LinearExpr _ -> sols
-        | PredVar (p, param) ->
-          List.fold_left (fun (params, predSol) (e, (pcoef, _)) ->
-            let params =
-              try assert (param = M.find p params); params
-              with Not_found -> M.add p param params in
-            let pcoef = M.map (fun x -> M.add x 1 M.empty) pcoef in
-            let expr = LTE, (assignParameters sol pcoef) in
-            let predSol = M.addDefault [] (fun a b -> b :: a) p expr predSol in
-
-            if k <> v && List.mem_assoc k components then
-              (* This vertex is a cutpoint. *)
-              let params', predSol' =
-                try
-                  let vv, _ = MV.find k subprobl in
-                  step (`A (e, vv)) expr false
-                with Not_found -> step (`B k) expr false in
-              (M.merge (fun _ -> maybeAdd (
-                fun x y -> assert (x = y); x)) params params'),
-              (M.merge (fun _ -> maybeAdd (@)) predSol predSol')
-            else params, predSol
-          ) sols pexprs
-      ) templ (M.empty, M.empty) in
-    step (`A (!rootE, rootV)) (LTE, M.empty) true in
-
-  let rec step st =
-    try
-      trySolve st
-    with Unsatisfiable x ->
-      let m = List.fold_left (fun m (ges, tag) ->
-        MEL.addDefault [] (fun a b -> b :: a) ges tag m
-      ) MEL.empty x in
-
-      let keys = MEL.fold (fun k _ s ->
-        let x = List.rev k in
-        match SEL.split k s with
-        | _, true, _ -> s
-        | sl, _, sr ->
-          let ret =
-            if SEL.is_empty sl then None
-            else
-              let slx = SEL.max_elt sl in
-              if not (List.starts_with slx x) then None
-              else Some (SEL.remove slx s |> SEL.add x) in
-          match ret with
-          | Some x -> x
-          | None ->
-            if SEL.is_empty sr then SEL.add x s
-            else
-              let srx = SEL.min_elt sr in
-              if List.starts_with srx x then s
-              else SEL.add x s) m SEL.empty in
-      let keys = SEL.fold (fun x l -> List.rev x :: l) keys [] in
-
-      let edge_check el dst e =
-        GV.E.label e = el && GV.V.label (GV.E.dst e) = dst in
-
-      let changed = ref false in
-      let rec grow st x v = function
-        | a::b::rest ->
-          let v' =
-            GV.succ_e st v |>
-            List.filter (edge_check a (G.E.dst b)) |>
-            List.hd |>
-            GV.E.dst in
-          grow st x v' (b::rest)
-        | [e] ->
-          if GV.succ_e st v |> List.exists (edge_check e x) then
-            st (* Do nothing. *)
-          else (
-            changed := true;
-            GV.add_edge_e st (GV.E.create v e (GV.V.create x))) in
-
-      let f_key st key =
-        (* Traverse st based on key. *)
-        let path = (!rootE :: key) in
-        let f_dst st dst = grow st dst rootV path in
-
-        MEL.find key m |>
-        List.fold_left (fun m ->
-          function
-          | Coef es -> List.fold_left (fun m e ->
-            MV.addDefault 0 (+) (G.E.dst e) 1 m) m es
-          | _ -> m) MV.empty |>
-        MV.filter (fun k v -> v > 1 && List.mem_assoc k components) |>
-        MV.keys |>
-        List.fold_left f_dst st in
-      let st = List.fold_left f_key st keys in
-      if not !changed then raise No_growth;
-      step st in
-  step st
+  let x = gen_constr MV.empty root [] in
+  assert false
 
 let simplifyPCNF clauses =
   let simplifyPDF exprs =
@@ -733,15 +496,17 @@ let solve clauses =
   print_newline ();
 
   (* Generate the problem graph. *)
-  let g = buildGraph clauses in
+  let g, root, preds = buildGraph clauses in
+  rootE := G.E.create root None root;
 
   (* We don't handle cyclic graphs. *)
   assert (not (Traverser.has_cycle g));
 
-  (* Generate constraints for solving the graph. *)
-  let g, root = findRoot g in
+  (* Create basic template for solution. *)
+  let sol_templ = List.fold_left (fun m k -> M.add k (Term k) m) M.empty preds in
 
-  let params, exprs = solveGraph (g, root) in
+  (* Generate constraints for solving the graph. *)
+  let params, exprs = solveGraph (g, root) sol_templ in
   let sol = M.merge (fun _ (Some param) (Some exprs) ->
       Some (param, (And (List.map (fun x -> Term x) exprs)))) params exprs in
 
