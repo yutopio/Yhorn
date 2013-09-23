1c65660f983a992790efd8c89bcc8a877d1885ac
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2013-09-23 13:44:45 +0900

    Fix PPL simplification

--- pplInterface.ml
+++ pplInterface.ml
@@ -11,7 +11,7 @@ open Util
 let int = Z.of_int
 let intc x = Coefficient (int x)
 
-let of_expr vm (op, coef) =
+let of_expr vl (op, coef) =
   let coef = M.filter (fun _ -> (<>) 0) coef in
   if M.cardinal coef = 0 then
     (* If no term is given, return the Boolean value. *)
@@ -28,11 +28,12 @@ let of_expr vm (op, coef) =
           if k = Id.const then intc v
           else
             let var =
-              if M.mem k !vm then
-                Variable (M.find k !vm)
+              let i = List.indexof k !vl in
+              if i <> -1 then
+                Variable i
               else
-                let id = M.cardinal !vm in
-                vm := M.add k id !vm;
+                let id = List.length !vl in
+                vl := !vl @ [k];
                 Variable id in
             if v = 1 then var
             else Times (int v, var)) :: l) x [] in
@@ -48,19 +49,19 @@ let of_expr vm (op, coef) =
     | GT -> Greater_Than (t1, t2)
     | GTE -> Greater_Or_Equal (t1, t2)
 
-let rec term_of vmi =
-  let f t1 g t2 = g (term_of vmi t1) (term_of vmi t2) in
+let rec term_of vl =
+  let f t1 g t2 = g (term_of vl t1) (term_of vl t2) in
   function
-  | Variable id -> M.add (MI.find id vmi) 1 M.empty
+  | Variable id -> M.add (List.nth vl id) 1 M.empty
   | Coefficient c -> M.add Id.const (Z.to_int c) M.empty
   | Plus (t1, t2) -> f t1 (++) t2
   | Minus (t1, t2) -> f t1 (--) t2
-  | Unary_Plus t -> term_of vmi t
-  | Unary_Minus t ->  ~-- (term_of vmi t)
-  | Times (c, v) -> M.map (fun x -> (Z.to_int c) * x) (term_of vmi v)
+  | Unary_Plus t -> term_of vl t
+  | Unary_Minus t ->  ~-- (term_of vl t)
+  | Times (c, v) -> M.map (fun x -> (Z.to_int c) * x) (term_of vl v)
 
-let expr_of vmi =
-  let f t1 t2 = (term_of vmi t1) -- (term_of vmi t2) in
+let expr_of vl =
+  let f t1 t2 = (term_of vl t1) -- (term_of vl t2) in
   function
   | Equal (t1, t2) -> EQ, f t1 t2
   | Less_Than (t1, t2) -> LT, f t1 t2
@@ -68,24 +69,25 @@ let expr_of vmi =
   | Greater_Than (t1, t2) -> GT, f t1 t2
   | Greater_Or_Equal (t1, t2) -> GTE, f t1 t2
 
-let integer_qelim vars la =
-  let vars = S.diff (fvs la) vars in
+let integer_qelim quants la =
+  let vars = S.diff (fvs la) quants in
   let la = Formula.normalize la in
-  let vm = ref M.empty in
+
+  let vl = ref [] in
   let constrs =
     match la with
     | And x ->
       List.map (
         function
-        | Term x -> of_expr vm x
+        | Term x -> of_expr vl x
         | _ -> failwith not_polyhedron) x
     | Or x -> failwith not_polyhedron
-    | Term x -> [of_expr vm x] in
-  let vm = !vm in
+    | Term x -> [of_expr vl x] in
+  let vl = !vl in
 
   let poly = ppl_new_C_Polyhedron_from_constraints constrs in
-  let dim = S.fold (fun x l -> (M.find x vm) :: l) vars [] in
+  let dim = S.fold (fun x l -> (List.index_of x vl) :: l) vars [] in
+  let _, vl = List.partition (fun x -> S.mem x vars) vl in
   ppl_Polyhedron_remove_space_dimensions poly dim;
   let constrs = ppl_Polyhedron_get_constraints poly in
-  let vmi = M.fold (fun k v m -> MI.add v k m) vm MI.empty in
-  And (List.map (fun x -> Term (expr_of vmi x)) constrs)
+  And (List.map (fun x -> Term (expr_of vl x)) constrs)
